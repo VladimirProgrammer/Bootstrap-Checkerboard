351205de3d9a204146326a03f934587a763e62b6
VladimirProgrammer <vova_progmush@mail.ru>
2021-01-18 21:53:17 +0300

    First commit

---
+++ README.m
@@ -0,0 +1,3 @@
+# Bootstrap-Checkerboard
+This the part of the course of Codecademy LEARN BOOTSTRAP 4: GRID
+The task was to create a 8 x 8 checkerboard, nest a layer of rows and columns inside a column, recreate the layout in the second exercise.
